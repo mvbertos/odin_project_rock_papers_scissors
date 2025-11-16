9073bc4d30ef48ce9c7083378da873d1fb90ca64
mvbertos <matheus.bertos2@gmail.com>
2025-11-16 14:00:07 -0300

    added game.js which has the pseudocode written on it

---
+++ game.js
@@ -0,0 +1,21 @@
+// Begin a round
+//it will be 5 rounds once the game starts
+
+// Write a gethumanchoice function
+// choose a option? 0:rock|1:paper|2:scissors
+// it will get a int input from 0 to 2
+// 0 is rock, 1 is paper and 2 is scissors
+// print back the choosen option
+
+// Write a getcomputer function
+// this function will randomly return 0,1 or 2
+// 0 is rock, 1 is paper and 2 is scissors
+// print back the function the computer selects
+
+// check who wins
+// 0 losses to 1, 1 losses to 2 and 2 losses to 0
+// print back the result: example(Rock beats scissors)
+// if computer wins one point to computer
+// if human wins one point to human
+
+// end the round if this is the 5th round already end the game
